aaba5131818059aa667dcc7208e393e75b6c5c8c
Prajakta <prajaktaghodake17@gmail.com>
2020-05-10 18:58:56 +0530

    My First Publisher issue #4

--- ex04_publisher/src/my_publisher.py
+++ ex04_publisher/src/my_publisher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy                            # Import the Python library for ROS
-from std_msgs.msg import Twist          # Import the Int32 message from the std_msgs
+from geometry_msgs.msg import Twist          # Import the Int32 message from the std_msgs
 
 rospy.init_node('vel_publisher')      # Initiate a Node named 'topic_publisher'
 pub = rospy.Publisher('/cmd_vel', Twist) # Create a Publisher object, that will publish
@@ -9,12 +9,11 @@ pub = rospy.Publisher('/cmd_vel', Twist) # Create a Publisher object, that will 
 
 rate = rospy.Rate(2)                    # Set a publish rate of 2 Hz
 twist = Twist()
-twist.linear.x = 0.1 
+twist.linear.x = 0.1
 twist.angular.z = 0                     # Create a var of type Int32
-                          
 
-while not rospy.is_shutdown():          # Create a loop that will go until someone stops the programm                  
+
+while not rospy.is_shutdown():          # Create a loop that will go until someone stops the programm
     pub.publish(twist)                  # Publish the message within the 'count' variable
                                         # Increment 'count' variable
     rate.sleep()                        # Make sure the publish rate maintains at 2 Hz
-
