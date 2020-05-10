f494b29d0cc2817287efb272009fac9bc6fc01f8
Prajakta <prajaktaghodake17@gmail.com>
2020-05-10 19:24:23 +0530

    Subscriber issue #5

--- ex05_vel_subscriber/src/my_subscriber.py
+++ ex05_vel_subscriber/src/my_subscriber.py
@@ -1,11 +1,12 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import rospy
-from geometry_msgs.msg import Twist
+import rospy                            # Import the Python library for ROS
+from geometry_msgs.msg import Twist          # Import the Int32 message from the std_msgs
 
 def callback(msg):
     print msg
 
 rospy.init_node('vel_subscriber')
-rospy.Subscriber('/cmd_vel',Twist, callback)
+rospy.Subscriber('/cmd_vel' , Twist , callback)
 rospy.spin()
+
