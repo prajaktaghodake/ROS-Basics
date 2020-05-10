4d2448d4ac2d87194dd4896ec87c61f90e7b1c76
Prajakta <prajaktaghodake17@gmail.com>
2020-05-10 18:44:02 +0530

    My First Package issue#2

--- ex01_my_first_package/src/first_node.py
+++ ex01_my_first_package/src/first_node.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 import rospy
+
 rospy.init_node('first_node')    
-print ("this is my first node")
+rate = rospy.Rate(2)
+
+while not rospy.is_shutdown():
+	print "this is my first node"
+	rate.sleep()
 
