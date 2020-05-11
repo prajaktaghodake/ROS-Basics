8e05066b3e5c8ef7bddd153562b6563985ee78df
Prajakta Ghodake <pg-192091@hs-weingarten.de>
2020-05-11 08:53:18 +0200

    ex07_launch.py

--- ex07_launch_file/src/ex07.py
+++ ex07_launch_file/src/ex07.py
@@ -4,14 +4,11 @@ import rospy
 from goal_publisher.msg import PointArray
 
 def callback(msg):
-    i=0
-    while (i<20):
-        #print(".*Point0 : x: ={a}, y:={b} ,z:={c}".format(a= msg.goals[i].x,b= msg.goals[i].y,c= msg.goals[i].z))
-        #print(".*Point1 : x: ={d}, y:={e} ,z:={f}".format(d= msg.goals[i].x+1,e=msg.goals[i].y+1,f=msg.goals[i].z))
-        #print(".*Point2 : x: ={g}, y:={h} ,z:={i}".format(g= msg.goals[i].x +2,h=msg.goals[i].y+2,i=msg.goals[i].z))
-        print("Point: {%0.2f}".format(msg.goals[i].x))
-        i=i+1
-
+    print("Point 0: x: {a}, y: {b}, z: {c}".format(a= msg.goals[0].x,b=msg.goals[0].y,c=msg.goals[0].z))
+    print("Point 1: x: {d}, y: {e}, z: {f}".format(d= msg.goals[1].x,e=msg.goals[1].y,f=msg.goals[1].z))
+    print("Point 2: x: {g}, y: {h}, z: {i}".format(g= msg.goals[2].x,h=msg.goals[2].y,i=msg.goals[2].z))
+    
+    
 rospy.init_node('goal_launch')
 rospy.Subscriber('/points', PointArray, callback)
 rospy.spin()
